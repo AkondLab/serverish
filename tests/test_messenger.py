04cccfbc62bff53c8f6a03835fe6faa96928ff9e
mka <mka@akond.com>
2023-08-05 01:35:39 +0200

    sub tests

--- tests/test_messenger.py
+++ tests/test_messenger.py
@@ -68,6 +68,73 @@ async def test_messenger_pub_sub():
         await asyncio.gather(subsciber_task(sub), publisher_task(pub))
         await pub.close()
         await sub.close()
+
+
+@pytest.mark.asyncio  # This tells pytest this test is async
+@pytest.mark.skipif(ci, reason="JetStreams Not working on CI")
+@pytest.mark.skipif(not is_nats_running(), reason="requires nats server on localhost:4222")
+async def test_messenger_pub_sub_pub():
+
+    now = datetime.datetime.now()
+    pub = await get_publisher('test.messenger')
+    # sub = await get_subscription('test.messenger', deliver_policy='by_start_time', opt_start_time=now)
+    sub = await get_subscription('test.messenger', deliver_policy='all')
+    collected = []
+    async def subsciber_task(sub):
+        async for msg in sub:
+            print(msg)
+            collected.append(msg)
+            if msg['final']:
+                break
+
+    async def publisher_task(pub, finalize=False):
+        await asyncio.sleep(0.1)
+        for i in range(10):
+            await pub.publish(data={'n': i, 'final': False})
+            await asyncio.sleep(0.1)
+        if finalize:
+            await pub.publish(data={'n': 10, 'final': True})
+
+    async with Messenger().context(host='localhost', port=4222):
+        await publisher_task(pub, finalize=False) # pre-publish 10
+        await asyncio.sleep(0.1)
+        # subscribe and publish 11 more
+        await asyncio.gather(subsciber_task(sub), publisher_task(pub, finalize=True))
+        await pub.close()
+        await sub.close()
+    assert len(collected) == 21
+
+
+async def test_messenger_pub_time_pub_sub():
+
+    pub = await get_publisher('test.messenger')
+    collected = []
+    async def subsciber_task(sub):
+        async for msg in sub:
+            print(msg)
+            collected.append(msg)
+            if msg['final']:
+                break
+
+    async def publisher_task(pub, finalize=False):
+        await asyncio.sleep(0.1)
+        for i in range(10):
+            await pub.publish(data={'n': i, 'final': False})
+            await asyncio.sleep(0.1)
+        if finalize:
+            await pub.publish(data={'n': 10, 'final': True})
+
+    async with Messenger().context(host='localhost', port=4222):
+        await publisher_task(pub, finalize=False) # pre-publish 10
+        await asyncio.sleep(0.1)
+        now = datetime.datetime.now()
+        await publisher_task(pub, finalize=False) # publish 11 more
+        sub = await get_subscription('test.messenger', deliver_policy='by_start_time', opt_start_time=now)
+        await subsciber_task(sub)
+        await pub.close()
+        await sub.close()
+    assert len(collected) == 11 # only the 11 published after `now`
+
 #
 # @pytest.mark.asyncio  # This tells pytest this test is async
 # @pytest.mark.skipif(not is_nats_running(), reason="requires nats server on localhost:4222")
